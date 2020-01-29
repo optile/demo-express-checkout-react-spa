bc5933d0aedd5dd02747caa3916330c84e5f3645
Vanderlei Alves da Silva <vanderlei.alves.da.silva@gmail.com>
2020-01-29 12:15:23 +0100

    fix redirect problem

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,10 +2,25 @@ import React from "react";
 import ExpressCheckout from "express-checkout";
 
 import getAttributes from "../configuration/spa";
+import { useHistory } from "react-router-dom";
 
 function Checkout() {
+  let history = useHistory();
   const attributes = getAttributes();
 
-  return <ExpressCheckout {...attributes} mode={null} longId={null} />;
+  const onProceed = ({ step }) => {
+    if (step === "update") {
+      history.push("/cancel");
+    }
+  };
+
+  return (
+    <ExpressCheckout
+      {...attributes}
+      customFunctions={{ onProceed }}
+      mode={null}
+      longId={null}
+    />
+  );
 }
 export default Checkout;
