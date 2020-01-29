b4d7859ef4a7e31fb7af605f18ced937323c5388
Joseph Elalam <joseph.elalam@optile.net>
2020-01-29 17:16:41 +0100

    Updates

---
+++ precache-manifest.f5e72cb7ca52acea104a48a818f35466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd04f15cde1d050abd39f3c247dd617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ec3174d4d6ca67277b9",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "469fa1d735fdc5cf6c71",
+    "url": "/static/js/2.c2082167.chunk.js"
+  },
+  {
+    "revision": "8e74e3b3c1da207b8ae670ab261e353f",
+    "url": "/static/js/2.c2082167.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ec3174d4d6ca67277b9",
+    "url": "/static/js/main.d88efcab.chunk.js"
+  },
+  {
+    "revision": "8fb8f32f6a48518950e9",
+    "url": "/static/js/runtime-main.3e24de51.js"
+  }
+]);
